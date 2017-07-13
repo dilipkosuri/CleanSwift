d02e679128f0ddeebeedcf06a2511501416f5fe6
DILIP KOSURI <kosuri.dilip@gmail.com>
2017-07-13 15:10:04 +0800

    Initial Code commit for Clean Architecture

---
+++ CleanSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanSwift
+//
+//  Created by SGpad MBP 019 on 13/7/17.
+//  Copyright © 2017 DILIP KOSURI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
